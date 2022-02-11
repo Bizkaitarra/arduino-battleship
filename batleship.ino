750229fa38b42aec21a166c1fa9c678a7219b7d9
Jon González <jon@clicars.com>
2022-02-11 15:27:27 +0100

    Buttons counting letters and numbers

--- batleship.ino
+++ batleship.ino
@@ -1,29 +1,39 @@
-const int firstButtonPin = 2;
-const int secondButtonPin = 3;
-
+const int letterButtonPin = 2;
+const int numbersButtonPin = 3;
+const int NUMBER_OF_LETTERS = 7;
 int value = 0;
+
+String letters[NUMBER_OF_LETTERS] = {"A", "B", "C", "D", "E", "F", "G"};
+int currentStringIndex=0;
+int currentNumber=0;
  
 void setup() {
   Serial.begin(9600);
-  pinMode(firstButtonPin, INPUT);
-  pinMode(secondButtonPin, INPUT);
+  pinMode(letterButtonPin, INPUT);
+  pinMode(numbersButtonPin, INPUT);
 }
  
 void loop(){
-  readButton(firstButtonPin);
-  readButton(secondButtonPin);
+  if (buttonIsPressed(letterButtonPin)) {
+    currentStringIndex++;
+    if (currentStringIndex == NUMBER_OF_LETTERS) {
+      currentStringIndex=0;
+    }
+    Serial.println("Letter");
+    Serial.println(currentStringIndex);    
+    Serial.println(letters[currentStringIndex]);
+  }
+  
+  if (buttonIsPressed(numbersButtonPin)) {
+    currentNumber++;
+    Serial.println("Number");
+    Serial.println(currentNumber);
+  } 
+  
+  delay(200);
 }
 
-void readButton(int buttonPin) {
-  value = digitalRead(buttonPin);  //lectura digital de pin
-
- Serial.println(buttonPin);
-  //mandar mensaje a puerto serie en función del valor leido
-  if (value == HIGH) {
-      Serial.println("Encendido pin");
-  }
-  else {
-      Serial.println("Apagado pin");
-  }
-  delay(300);
+bool buttonIsPressed(int buttonPin) {
+  value = digitalRead(buttonPin);  
+  return value == HIGH;
 }
